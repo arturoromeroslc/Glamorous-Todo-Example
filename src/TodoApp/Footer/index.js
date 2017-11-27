2ba0a82bb658d79441ca187899bb923887127ae1
Arturo Romero <arturo@Arturos-MacBook-Pro.local>
2017-11-26 16:20:53 -0800

    simple glamour change

--- src/TodoApp/Footer/index.js
+++ src/TodoApp/Footer/index.js
@@ -1,32 +1,37 @@
 import React from 'react'
 import FilterLink from './FilterLink'
+import glamorous from 'glamorous'
+
+const Span = glamorous.span({
+  marginRight: '5px'
+})
 
 export default ({
   visibilityFilter,
   onFilterClick
 }) => (
   <p>
-    Show:
+    <Span>Show:</Span>
     <FilterLink
       filter="SHOW_ALL"
       currentFilter={visibilityFilter}
       onClick={onFilterClick}
     >
-      ALL
+    <Span>ALL</Span>
     </FilterLink>
     <FilterLink
       filter="SHOW_ACTIVE"
       currentFilter={visibilityFilter}
       onClick={onFilterClick}
     >
-      ACTIVE
+    <Span>ACTIVE</Span>
     </FilterLink>
     <FilterLink
       filter="SHOW_COMPLETED"
       currentFilter={visibilityFilter}
       onClick={onFilterClick}
     >
-      COMPLETED
+    <Span>COMPLETED</Span>
     </FilterLink>
   </p>
 )
